e530f635122b390cf745f11056d60e50a2002e7d
oscarpe979 <oscarpe979@hotmail.com>
2025-09-28 18:04:10 +1000

    feat(backend): enable CORS and update Dockerfile for reload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import user
 
 app = FastAPI()
+
+# Set up CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",  # Default for Vite
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router, prefix="/api/v1", tags=["users"])
